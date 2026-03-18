327f43d249a70b73333488f3e90b4a539cf5245e
harlow-sint <255205578+harlow-sint@users.noreply.github.com>
2026-03-18 00:42:44 -0400

    add VBox vertical layout container

---
+++ lemonui/layout/VBox.hx
@@ -0,0 +1,29 @@
+package lemonui.layout;
+
+import flixel.FlxSprite;
+import lemonui.core.ElementBase;
+
+class VBox extends ElementBase {
+
+    public var spacing:Float;
+
+    public function new(x:Float = 0, y:Float = 0, spacing:Float = 0) {
+        super(x, y);
+        this.spacing = spacing;
+    }
+
+    public function addElement(element:FlxSprite) {
+        add(element);
+        reflow();
+    }
+
+    public function reflow() {
+        var currentY:Float = 0;
+        for (member in members) {
+            if (member == null) continue;
+            member.y = currentY;
+            member.x = 0;
+            currentY += member.height + spacing;
+        }
+    }
+}
