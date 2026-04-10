84e83f2714cf58d7d5efd419535db97e9441d2c2
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-04-10 23:59:09 +0700

    oop, one more

--- lemonui/Constants.hx
+++ lemonui/Constants.hx
@@ -1,6 +1,15 @@
 package lemonui;
 
-class Constants {
-    public static var FONT_REGULAR:String = "";
-    public static var FONT_BOLD:String = "";
+import lemonui.themes.ThemeManager;
+
+class Constants
+{
+	public static var FONT_REGULAR(get, never):String;
+	public static var FONT_BOLD(get, never):String;
+
+	static function get_FONT_REGULAR():String
+		return ThemeManager.fontRegular;
+
+	static function get_FONT_BOLD():String
+		return ThemeManager.fontBold;
 }
